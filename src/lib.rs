dbdff7e8df39b884d53286ba0edfb9184a7349b5
Luick Klippel <luickklippel@Luicks-MacBook-Pro.local>
2022-03-27 12:57:57 +0200

    implmenting basic local key/val store caching functionality

--- src/lib.rs
+++ src/lib.rs
@@ -1,45 +1,89 @@
 use std::io::prelude::*;
 use std::thread;
-use std::net::{TcpStream, TcpListener};
+use std::net::{TcpStream, TcpListener, SocketAddr};
 
-fn client_handler(mut socket: TcpStream){
-    let mut buf = [0; 10];
-    socket.read(&mut buf);
-    socket.write(&[1]);
-
-    println!("read: {:?}", buf);
+pub struct KeyValObj<KeyT, ValT> {
+    key: KeyT,
+    val: ValT,
 }
 
-fn server_client_handler() {
-    let listener = TcpListener::bind("127.0.0.1:1337").unwrap();
-    let mut handlers = Vec::new();
+pub struct CacheDb<KeyT, ValT> {
+    ipv4_addr: [u8; 4],
+    port: u16,
+    cache_size: u16,
 
-    loop {
-        match listener.accept() {
-            Ok((socket, addr)) => {
-                println!("new client: {:?}", addr);
+    key_val_store: Vec<Box<KeyValObj<KeyT, ValT>>>
+}
 
-                let handler = thread::spawn(move || {
-                    client_handler(socket);
-                });
-                handlers.push(handler);
-            }
-            Err(e) => println!("couldn't get client: {:?}", e),
-        }    
+impl<KeyT, ValT> CacheDb<KeyT, ValT> {
+
+    pub fn new(ipv4_addr: [u8;4], port: u16, cache_size: u16) -> CacheDb<KeyT, ValT> {
+        let cache = CacheDb {
+            ipv4_addr: ipv4_addr, 
+            port: port,
+            cache_size: cache_size,
+            key_val_store: Vec::new()
+        };
+        cache
     }
-}
 
-fn client_process() {
-    let mut stream = TcpStream::connect("127.0.0.1:1337").unwrap();
-    stream.write(&[1]).unwrap();
+    pub fn push(&mut self, obj: KeyValObj<KeyT, ValT>) {
+        self.key_val_store.push(Box::<KeyValObj<KeyT, ValT>>::new(obj));
+    }
+
+    pub fn get(&self, key: KeyT) -> Option<&KeyValObj<KeyT, ValT>> {
+        for obj in self.key_val_store.iter() {
+            if obj.key == key {
+                return Some(&obj);
+            }
+        }
+        None   
+    }
+
+    fn client_handler(mut socket: TcpStream){
+        let mut buf = [0; 10];
+        socket.read(&mut buf).unwrap();
+        socket.write(&[1]).unwrap();
+
+        println!("read: {:?}", buf);
+    }
+
+    pub fn server_client_handler(&self) {
+        let addr = SocketAddr::from((self.ipv4_addr, self.port));
+        let listener = TcpListener::bind(addr).unwrap();
+
+        loop {
+            match listener.accept() {
+                Ok((socket, addr)) => {
+                    println!("new client: {:?}", addr);
+
+                    let _handler = thread::spawn(move || (CacheDb::<KeyT, ValT>::client_handler(socket)));
+                }
+                Err(e) => println!("couldn't get client: {:?}", e),
+            }    
+            #[cfg(test)] 
+            break;
+        }
+    }   
 }
 
 
 #[cfg(test)]
 mod tests {
+    use std::{thread, time};
+
+    use super::*;
+
+    fn basic_client_test() {
+        let mut stream = TcpStream::connect("127.0.0.1:8080").unwrap();
+        stream.write(&[1]).unwrap();
+    }
+
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn server_test() { 
+        let cache = CacheDb::<String, String>::new([127, 0, 0, 1], 8080, 200);
+        let _test_server_instance = thread::spawn(move || (cache.server_client_handler()));
+        thread::sleep(time::Duration::from_secs(1));
+        basic_client_test();
     }
 }
